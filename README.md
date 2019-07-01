500009cd7f235c2993ae6aafcbfbaa74cddd8e8f
SolonJones <imisuzu0@hotmail.com>
2019-07-01 16:18:27 +0800

    init new folder and create first review program

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the collection of sample code from CS 165 book 
+All document is organized by chapter and section 
+
---
+++ chapter8/8-24.cpp
@@ -0,0 +1,7 @@
+// This is demonstrating the usage of vector 
+
+#include <iostream>
+#include <iomanip>
+// why need iomanip 
+
+
